4f1adb95d7e6fb7999c2bdb23c4744b1d7e1e06e
Kimberly Seria <kseria@gms.ca>
2020-03-23 14:50:51 -0600

    initial POC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { CardElementComponent } from './card-element/card-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CardElementComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [CardElementComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    // using createCustomElement from angular package it will convert angular component to stander web component
+    const el = createCustomElement(CardElementComponent, {
+      injector: this.injector
+    });
+
+    // using built in the browser to create your own custome element name
+    customElements.define('facebook-card', el);
+  }
+
+  ngDoBootstrap() { }
+}
