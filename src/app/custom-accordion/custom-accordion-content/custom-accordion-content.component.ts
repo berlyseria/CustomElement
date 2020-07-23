5d7c3e1f541d557f66da325b8e746186f20bd289
Kimberly Seria <kseria@gms.ca>
2020-07-23 11:42:34 -0600

    more custom accordion

--- src/app/custom-accordion/custom-accordion-content/custom-accordion-content.component.ts
+++ src/app/custom-accordion/custom-accordion-content/custom-accordion-content.component.ts
@@ -5,7 +5,9 @@ import {
   Input,
   Output,
   ViewEncapsulation,
-  AfterViewInit
+  AfterViewInit,
+  ViewChildren,
+  ViewChild
 } from '@angular/core';
 
 @Component({
@@ -19,18 +21,19 @@ export class CustomAccordionContentComponent implements AfterViewInit {
   private _contentId: number;
   private _isopen: boolean = false;
 
-  @Input() set contentId(value: number) {
-    this._contentId = value;
-  }
-  get contentId(): number {
-    return this._contentId;
-  }
+  @Input() set contentId(value: number) { this._contentId = value; }
+  get contentId(): number { return this._contentId; }
 
   @Output() toggleAccordion: EventEmitter<boolean> = new EventEmitter();
 
+  @ViewChild('accordion') _accordionEl: ElementRef;
+
   toggleOpen() {
-    // this.isopen = !this.isopen;
+    this.isopen = !this.isopen;
     console.log("from accordion-content", this.isopen);
+    console.log(this._accordionEl.nativeElement);
+    this._accordionEl.nativeElement.classList.toggle("active");
+
     this.toggleAccordion.emit(this.isopen);
   }
 
@@ -39,9 +42,5 @@ export class CustomAccordionContentComponent implements AfterViewInit {
 
   constructor(public elementRef: ElementRef) { }
 
-  ngAfterViewInit(): void {
-    if (this.isopen) {
-      this.toggleOpen();
-    }
-  }
+  ngAfterViewInit(): void { }
 }
